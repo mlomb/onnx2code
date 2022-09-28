18e57dd394d73468c67a32335196e4691b72346a
Sponja- <tomas.spognardi@gmail.com>
2022-09-28 13:44:45 -0300

    fixed some type errors in ops/ files

--- onnx2code/ops/identity.py
+++ onnx2code/ops/identity.py
@@ -1,3 +1,4 @@
+from ..generator import Generator
 from .operation import Operation
 
 
@@ -10,17 +11,17 @@ class Identity(Operation):
 
     node_types = ["Identity"]
 
-    def asserts(self):
+    def asserts(self) -> None:
         assert len(self.inputs) == 1, "expected one input"
         assert len(self.outputs) == 1, "expected one output"
         assert (
-            self.inputs[0]["size"] == self.outputs[0]["size"]
+            self.inputs[0].size == self.outputs[0].size
         ), "input and output tensors should have the same size"
 
 
 @Identity.variant("cpp")
 class IdentityCPP(Identity):
-    def generate(self, gen):
+    def generate(self, gen: Generator) -> None:
         gen.add_c_block(
             """
         template<typename T, unsigned int SIZE>
@@ -37,5 +38,5 @@ class IdentityCPP(Identity):
 
 @Identity.variant("asm")
 class IdentityASM(Identity):
-    def generate(self):
+    def generate(self, gen: Generator) -> None:
         print("ASM variant called")
--- onnx2code/ops/operation.py
+++ onnx2code/ops/operation.py
@@ -1,18 +1,26 @@
 from abc import ABC, abstractmethod
-from collections.abc import Generator
+import collections.abc
 from typing import Callable
+
+import onnx
+
+from ..generator import Generator
 
 
 class Operation(ABC):
     node_types: list[str]
     variant_name: str | None = None
 
-    def __init__(self, node):
+    def __init__(self, node: onnx.NodeProto):
         self.node = node
         self.asserts()
 
     @abstractmethod
     def asserts(self) -> None:
+        pass
+
+    @abstractmethod
+    def generate(self, gen: Generator) -> None:
         pass
 
     @classmethod
@@ -27,7 +35,7 @@ class Operation(ABC):
     @classmethod
     def get_subclasses(
         cls: type["Operation"],
-    ) -> Generator[type["Operation"], None, None]:
+    ) -> collections.abc.Generator[type["Operation"], None, None]:
         for subclass in cls.__subclasses__():
             yield from subclass.get_subclasses()
             yield subclass
