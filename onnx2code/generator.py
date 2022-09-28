86e9430a4c495fb98edd108122113aad049616d0
Sponja- <tomas.spognardi@gmail.com>
2022-09-28 18:25:41 -0300

    added tensor name indexing and weld_tensors function

--- onnx2code/generator.py
+++ onnx2code/generator.py
@@ -11,4 +11,16 @@ class Generator:
 
     def __init__(self, model_proto: onnx.ModelProto):
         self.model_proto = model_proto
-        self.tensors = parse_tensors(model_proto)
+        self.tensors = {tensor.name: tensor for tensor in parse_tensors(model_proto)}
+
+    def weld_tensors(self, name_from: str, name_to: str) -> None:
+        """
+        Weld tensors together
+        This means they should point to the same variable in runtime
+
+        :param name_from: Name of the origin tensor
+        :param name_to: Name of the destination tensor
+        :raises KeyError: If the tensor names are not found
+        """
+
+        self.tensors[name_from].variable = self.tensors[name_to].variable
