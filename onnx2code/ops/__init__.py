36222723eaf3f69671b713411407c60c935cef64
Sponja- <tomas.spognardi@gmail.com>
2022-09-29 01:53:34 -0300

    remove unused test

--- onnx2code/ops/__init__.py
+++ onnx2code/ops/__init__.py
@@ -1,7 +1,2 @@
 from . import identity
 from . import operation
-
-for cls in operation.Operation.get_subclasses():
-    if hasattr(cls, "variant"):
-        print(cls, cls.node_types, cls.variant)
-
