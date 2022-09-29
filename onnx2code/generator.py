1ca1a9b174b7f69538860ff0d1d21a0f12e60236
Sponja- <tomas.spognardi@gmail.com>
2022-09-29 00:09:02 -0300

    sorted imports
    Co-authored-by: nachovigilante <nachovigilante@gmail.com>
    Co-authored-by: mlomb <to@mlomb.me>

--- onnx2code/generator.py
+++ onnx2code/generator.py
@@ -1,4 +1,5 @@
 import onnx
+
 from .tensor import parse_tensors
 
 
--- onnx2code/ops/operation.py
+++ onnx2code/ops/operation.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
 import collections.abc
+from abc import ABC, abstractmethod
 from typing import Callable
 
 import onnx
