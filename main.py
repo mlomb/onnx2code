7edf9211ed8cdb23671f32e192f069e25256194d
Martín Lombardo <to@mlomb.me>
2022-10-03 02:17:53 -0300

    model service idea

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from onnx2code.generator import Generator
 from onnx2code.model_check import model_check
+from onnx2code.service import ModelService
 
 # Identity test model
 inputs = tf.keras.Input([1])
@@ -10,8 +11,15 @@ outputs = tf.keras.layers.Lambda(lambda x: x)(inputs)
 model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
 model_proto, _ = tf2onnx.convert.from_keras(model)
 
-# quick test
+# generate
 output = Generator(model_proto).generate()
 print(output)
+
+# model service?
+with ModelService(output) as service:
+    print(service)
+    service.inference(inputs=[])
+
+# quick test
 correct = model_check(output, n_inputs=1)
 print(f"Model check: {correct}")
--- onnx2code/model_check.py
+++ onnx2code/model_check.py
@@ -1,9 +1,5 @@
-from pathlib import Path
-from subprocess import call
-
 from .output import Output
-
-CHECKER_PATH = Path(__file__).parent / "model_check.c"
+from .service import ModelService
 
 
 def model_check(output: Output, n_inputs: int = 2) -> bool:
@@ -13,48 +9,10 @@ def model_check(output: Output, n_inputs: int = 2) -> bool:
     <n_inputs> random inputs will be generated
     """
 
-    tmp_folder = Path("tmp/")
-    tmp_folder.mkdir(exist_ok=True)
-    cpp_file = tmp_folder / "model.cpp"
-    hpp_file = tmp_folder / "model.hpp"
-    asm_file = tmp_folder / "model.asm"
+    with ModelService(output) as service:
+        for _ in range(n_inputs):
+            # TODO: generar inputs
+            service.inference(inputs=[])
+            # invocar a onnxruntime y comparar!
 
-    asm_object = tmp_folder / "model-asm.o"
-    final_object = tmp_folder / "model-check"
-
-    with open(cpp_file, "w") as f:
-        f.write(output.source_cpp)
-    with open(hpp_file, "w") as f:
-        f.write(output.source_hpp)
-    with open(asm_file, "w") as f:
-        f.write(output.source_asm)
-
-    run_commands(
-        [
-            ["nasm", "-f", "elf64", str(asm_file), "-o", str(asm_object), "-g"],
-            [
-                "gcc",
-                "-m64",  # 64 bit env
-                str(asm_object),
-                str(hpp_file),
-                str(cpp_file),
-                str(CHECKER_PATH),
-                "-o",
-                str(final_object),
-                "-O0",
-                "-g",
-                "-fsanitize=address",
-            ],
-        ]
-    )
-
-    exit_code = call([str(final_object), str(n_inputs)])
-    
-    return exit_code == 0
-
-
-def run_commands(cmds: list[list[str]]) -> None:
-    for cmd in cmds:
-        exit_code = call(cmd)
-        if exit_code != 0:
-            raise Exception(f"Command failed: {cmd}")
+    return False
