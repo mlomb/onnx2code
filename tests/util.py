109752119d7e198c7b1cbff9b98431a275b656cd
Martín Lombardo <to@mlomb.me>
2022-10-07 23:02:09 -0300

    do not write model for debugging

--- tests/util.py
+++ tests/util.py
@@ -7,6 +7,6 @@ from onnx2code.checker import check_model
 
 def check_keras(model: tf.keras.Model) -> None:
     model_proto, _ = tf2onnx.convert.from_keras(model)
-    if True:
+    if False:
         onnx.save_model(model_proto, "tmp/model.onnx")
     check_model(model_proto)
