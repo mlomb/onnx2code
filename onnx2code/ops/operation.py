0206afbb0cb33c50d57216afac426db6e028a3d1
Martín Lombardo <to@mlomb.me>
2022-10-07 22:13:50 -0300

    simplified (and fixed) operation lookup

--- onnx2code/ops/operation.py
+++ onnx2code/ops/operation.py
@@ -8,9 +8,7 @@ from ..generator import Generator
 
 class Operation(ABC):
     node_types: set[str]
-    variant_name: str | None = None
-    variants: dict[str, type["Operation"]] = {}
-    _registry: dict[str, type["Operation"]] = {}
+    _registry: dict[str, dict[str, type["Operation"]]] = {}
 
     def __init__(self, gen: Generator, node: onnx.NodeProto):
         self.node = node
@@ -29,32 +27,21 @@ class Operation(ABC):
     @classmethod
     def variant(cls, name: str) -> Callable[[type["Operation"]], type["Operation"]]:
         def decorator(newcls: type[Operation]) -> type[Operation]:
-            newcls.node_types = cls.node_types
-            newcls.variant_name = name
-            cls.variants[name] = newcls
+            if cls.__name__ not in cls._registry:
+                cls._registry[cls.__name__] = {}
+            cls._registry[cls.__name__][name] = newcls
 
             return newcls
 
         return decorator
 
-    def __init_subclass__(cls) -> None:
-        if cls.variant_name is None:
-            for node_type in cls.node_types:
-                Operation._registry[node_type] = cls
-
     @staticmethod
-    # TODO: ver, hay un bug acá
-    # creo que baseclass esta mal seteado
     def get(node_type: str, variant: list[str]) -> type["Operation"]:
         if node_type not in Operation._registry:
             raise NotImplementedError(f"Operation {node_type} not implemented")
 
-        basecls = Operation._registry[node_type]
-
         for variant_name in variant:
-            return basecls.variants[variant_name]
+            if variant_name in Operation._registry[node_type]:
+                return Operation._registry[node_type][variant_name]
 
-        if len(basecls.variants) > 0:
-            raise ValueError(f"No valid variant found for {node_type}")
-
-        return basecls
+        raise ValueError(f"No valid variant found for {node_type}")
