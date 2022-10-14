7e00ec7c1bc77c6e44b860b677de52ceeeaee011
Martín Lombardo <to@mlomb.me>
2022-10-14 16:49:18 -0300

    test
    Co-Authored-By: Sponja <10713113+Sponja-@users.noreply.github.com>
    Co-Authored-By: nachovigilante <26276931+nachovigilante@users.noreply.github.com>

---
+++ tests/models/test_vision_classification.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+import onnx
+import pytest
+
+from onnx2code.checker import check_model
+
+
+@pytest.mark.parametrize("variation", ["c"])
+def test_mnist(variation: str) -> None:
+    model_proto = onnx.load(Path(os.path.dirname(__file__)) / "../../data/mnist-7.onnx")
+    check_model(model_proto, [variation])
