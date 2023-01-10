748501216f1e3cb62fd24091d75ec792a556ca04
Martín Lombardo <to@mlomb.me>
2023-01-10 03:31:00 -0300

    Clip operator fix
    Co-Authored-By: nachovigilante <26276931+nachovigilante@users.noreply.github.com>

--- onnx2code/ops/elementwise.py
+++ onnx2code/ops/elementwise.py
@@ -15,10 +15,16 @@ class Elementwise(Operation):
 
     def parse(self) -> None:
         assert len(self.outputs) == 1, "expected one output"
+
         for input in self.inputs:
             assert (
                 input.size == self.outputs[0].size
             ), "input and output tensors should have the same size"
+
+            if self.node.op_type == "Clip":
+                # Clip may have min and max as inputs
+                # or as attributes (depending on ONNX opset)
+                break
 
         self.op: str = self.node.op_type
         self.size = self.inputs[0].size
@@ -47,10 +53,22 @@ class ElementwiseC(Elementwise):
             case "Sigmoid":
                 impl = "1.0f / (1.0f + exp(-A[i]))"
             case "Clip":
-                finfo = np.finfo(dtype=np.float32)
-                min = get_attribute(self.node, "min", finfo.min)
-                max = get_attribute(self.node, "max", finfo.max)
-                impl = "A[i] < {} ? {} : A[i] > {} ? {} : A[i]".format(
+                if len(self.inputs) == 3:
+                    min_data = self.inputs[1].data
+                    max_data = self.inputs[2].data
+
+                    if min_data is None or max_data is None:
+                        raise ValueError("Clip: min and max should be constants")
+
+                    # "cast" the 0-dimensional arrays to numbers
+                    min = min_data + 0
+                    max = max_data + 0
+                else:
+                    finfo = np.finfo(dtype=np.float32)
+                    min = get_attribute(self.node, "min", finfo.min)
+                    max = get_attribute(self.node, "max", finfo.max)
+
+                impl = "A[i] < {} ? {} : (A[i] > {} ? {} : A[i])".format(
                     min, min, max, max
                 )
             case _:
