dd90aeff2312f2e88ed7cd7d3e45503c6062ce5b
Martín Lombardo <to@mlomb.me>
2023-01-10 03:31:10 -0300

    GEMM support for transB
    Co-Authored-By: nachovigilante <26276931+nachovigilante@users.noreply.github.com>

--- onnx2code/ops/gemm.py
+++ onnx2code/ops/gemm.py
@@ -29,14 +29,30 @@ class GEMM(Operation):
         self.beta = None if self.beta == 1.0 else self.beta
 
         assert not self.transA, "transA not supported"
-        assert not self.transB, "transB not supported"
         assert self.alpha is None, "alpha not supported"
         assert self.beta is None, "beta not supported"
+
+        A = self.inputs[0]
+        B = self.inputs[1]
+        Y = self.outputs[0]
+
+        self.N = A.shape[0]
+        self.M = B.shape[1] if self.transB else B.shape[0]
+        self.K = B.shape[0] if self.transB else B.shape[1]
+
+        assert Y.shape[0] == self.N
+        assert Y.shape[1] == self.K
 
     def call(self) -> OpCall:
         return OpCall(
             name="GEMM",
-            sig_params=[self.hasC, self.inputs[0].shape, self.inputs[1].shape],
+            sig_params=[
+                self.hasC,
+                self.N,
+                self.M,
+                self.K,
+                self.transB,
+            ],
             params=["A", "B"] + (["C"] if self.hasC else []) + ["Y"],
             inputs=self.inputs,
             outputs=self.outputs,
@@ -46,16 +62,16 @@ class GEMM(Operation):
 @GEMM.variant("c")
 class GEMMC(GEMM):
     def impl(self) -> OpImpl:
-        N = self.inputs[0].shape[0]
-        M = self.inputs[0].shape[1]
-        K = self.inputs[1].shape[1]
+        N, M, K = self.N, self.M, self.K
+
+        index_B = f"i * {K} + col" if self.transB == False else f"col * {M} + i"
 
         source = f"""
         for(int row = 0; row < {N}; row++) {{
             for(int col = 0; col < {K}; col++) {{
                 float sum = 0;
                 for(int i = 0; i < {M}; i++) {{
-                    sum += A[row * {M} + i] * B[i * {K} + col];
+                    sum += A[row * {M} + i] * B[{index_B}];
                 }}
                 Y[row * {K} + col] = sum{f' + C[row * {K} + col]' if self.hasC else ''};
             }}
