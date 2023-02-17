647efac9d7a9f745715779493bb640e9e03b340a
Martín Lombardo <to@mlomb.me>
2023-02-17 19:36:16 -0300

    fix format

--- evaluation/eval_gemm.py
+++ evaluation/eval_gemm.py
@@ -2,7 +2,6 @@ import setup  # noqa # isort:skip
 
 import keras
 import matplotlib.pyplot as plt
-import tensorflow as tf
 from keras import layers
 from measure import measure_all
 import numpy as np
