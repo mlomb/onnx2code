a96051a380bb9eb1c60dddcf8a498bfd6b0532e6
Martín Lombardo <to@mlomb.me>
2023-03-20 14:04:02 -0300

    ConvIm2col

--- onnx2code/ops/conv.py
+++ onnx2code/ops/conv.py
@@ -1,3 +1,6 @@
+from math import ceil
+import numpy as np
+from onnx2code.ops.gemm_tiling.GEMM import call_GEMM, external_paths_GEMM
 from onnx2code.util import (
     compute_strides,
     get_attribute,
@@ -34,6 +37,7 @@ class Conv(Operation):
         self.B = self.inputs[2] if len(self.inputs) == 3 else None
         self.Y = self.outputs[0]
 
+        self.dilations = get_attribute(self.node, "dilations", [1] * 2)
         self.strides = resolve_stride_attribute(self.node)
         self.pads = resolve_padding_attribute(self.node, self.X.shape, self.W.shape)
 
@@ -47,7 +51,7 @@ class Conv(Operation):
         )
 
 
-@Conv.variant("c")
+@Conv.variant("c", priority=1)
 class ConvC(Conv):
     def impl(self) -> OpImpl:
         # onnx is NCHW
@@ -109,3 +113,97 @@ class ConvC(Conv):
         """
 
         return OpImpl(lang="c", source=source)
+
+
+@Conv.variant(["im2col", "loop-tiling"], priority=0)
+class ConvIm2col(Conv):
+    def impl(self) -> OpImpl:
+        input_shape = self.X.shape
+        weight_shape = self.W.shape
+        has_bias = self.B is not None
+        pads, dilations, strides = self.pads, self.dilations, self.strides
+
+        assert len(pads) == 4 or np.allclose(
+            pads, 0
+        ), "expected padding only in two dimensions"
+
+        # onnx is NCHW
+        N = input_shape[0]
+        C = input_shape[1]
+        H = input_shape[2]
+        W = input_shape[3]
+        F = weight_shape[0]  # filters
+        KC = weight_shape[1]
+        KH = weight_shape[2]
+        KW = weight_shape[3]
+
+        input_strides = compute_strides(input_shape)
+        kernel_strides = compute_strides([KC, KH, KW])
+        pads_start = [pads[0], pads[1]]
+        pads_end = [pads[2], pads[3]]
+        patch_stride = KC * KH * KW
+        num_patches = ceil(
+            (H - KH + 1 + pads_start[0] + pads_end[0] - (dilations[0] - 1) * (KH - 1))
+            / strides[0]
+        ) * ceil(
+            (W - KW + 1 + pads_start[1] + pads_end[1] - (dilations[1] - 1) * (KW - 1))
+            / strides[0]
+        )
+
+        bias_code = (
+            ""
+            if has_bias
+            else f"""
+        // bias
+        for (int f = 0; f < {F}; f++) {{
+            for (int i = 0; i < {num_patches}; i++) {{
+                output[f * {num_patches} + i] += biases[f];
+            }}
+        }}
+        """
+        )
+
+        source = f"""
+        // padding, dilations, strides
+        // im2col
+        float im2col[200000];
+        int patch = 0;
+        for(int c = 0; c < {C - KC + 1}; c++) {{
+            for(int h = {-pads_start[0]}; h < {H - KH + 1 + pads_end[0] - (dilations[0] - 1) * (KH - 1)}; h += {strides[0]}) {{
+                for(int w = {-pads_start[1]}; w < {W - KW + 1 + pads_end[1] - (dilations[1] - 1) * (KW - 1)}; w += {strides[1]}) {{
+                    // copy patch
+                    for(int cc = 0; cc < {KC}; cc++) {{
+                        for(int hh = 0; hh < {KH}; hh++) {{
+                            for(int ww = 0; ww < {KW}; ww++) {{
+                                const int ih = h + hh * {dilations[0]};
+                                const int iw = w + ww * {dilations[1]};
+                                float value;
+                                if(ih < 0 || ih >= {H} || iw < 0 || iw >= {W}) {{
+                                    value = 0.0f;
+                                }} else {{
+                                    value = X[
+                                        (c + cc) * {input_strides[1]} +
+                                        ih * {input_strides[2]} +
+                                        iw * {input_strides[3]}
+                                    ];
+                                }}
+                                im2col[
+                                    (cc * {kernel_strides[0]} +
+                                    hh * {kernel_strides[1]} +
+                                    ww * {kernel_strides[2]}) * {num_patches} +
+                                    patch
+                                ] = value;
+                            }}
+                        }}
+                    }}
+                    patch++;
+                }}
+            }}
+        }}
+        // gemm TODO: FALTA COMPLETAR ESTOS 3 NUMEROS
+        {call_GEMM(0,0,0,"im2col, W, OUT")}
+        // bias
+        {bias_code}
+        """
+
+        return OpImpl(lang="c", source=source, external_paths=external_paths_GEMM)
