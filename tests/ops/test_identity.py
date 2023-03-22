762cafed00587996b8ec30dbf1c739460a806bfe
Martín Lombardo <to@mlomb.me>
2023-03-22 17:48:54 -0300

    disable identity asm variant

--- tests/ops/test_identity.py
+++ tests/ops/test_identity.py
@@ -5,7 +5,7 @@ from ..util import check_keras
 
 
 @pytest.mark.parametrize("shape", [[1], [2, 3], [4, 5, 6]])
-@pytest.mark.parametrize("variation", ["c", "asm"])
+@pytest.mark.parametrize("variation", ["c"]) # , "asm"
 def test_identity(variation: str, shape: list[int]) -> None:
     input = tf.keras.Input(shape)
     out = tf.keras.layers.Lambda(lambda x: x)(input)
